d21bc1db0af0f086bad03c212ebdd19f1b3bbe31
Matt Jacobs <mjacobs@netflix.com>
2016-04-06 10:40:22 -0700

    Java8: Replace lambdas with method references

--- hystrix-core/src/main/java/com/netflix/hystrix/metric/consumer/RollingDistributionStream.java
+++ hystrix-core/src/main/java/com/netflix/hystrix/metric/consumer/RollingDistributionStream.java
@@ -65,10 +65,10 @@ public class RollingDistributionStream<Event extends HystrixEvent> {
         }
     };
 
-    private static final Func1<Histogram, CachedValuesHistogram> cacheHistogramValues = histogram -> CachedValuesHistogram.backedBy(histogram);
+    private static final Func1<Histogram, CachedValuesHistogram> cacheHistogramValues = CachedValuesHistogram::backedBy;
 
     private static final Func1<Observable<CachedValuesHistogram>, Observable<List<CachedValuesHistogram>>> convertToList =
-            windowOf2 -> windowOf2.toList();
+            Observable::toList;
 
     protected RollingDistributionStream(final HystrixEventStream<Event> stream, final int numBuckets, final int bucketSizeInMs,
                                         final Func2<Histogram, Event, Histogram> addValuesToBucket) {
